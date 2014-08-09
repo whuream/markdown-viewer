da5d92b58408cdbe8250de69653a6f75c709146c
Thiht <thiht44@gmail.com>
2014-08-09 23:07:34 +0200

    Dynamic title based on the <hx> titles or on the first <p> found

--- data/markdown-viewer.js
+++ data/markdown-viewer.js
@@ -1,3 +1,19 @@
-var markedContent = marked(document.body.textContent);
+// Markdown to HTML
+var markedContent       = marked(document.body.textContent.trim());
 document.body.innerHTML = markedContent;
+
+// Title
+var firstTextBlock = document.querySelector("h1") || document.querySelector("h2") || document.querySelector("h3") || document.querySelector("h4") || document.querySelector("h5") || document.querySelector("h6") || document.querySelector("p");
+if (firstTextBlock !== null) {
+	document.title = firstTextBlock.textContent.trim().substr(0, 50);
+}
+
+// Syntax hightlighting
 hljs.initHighlighting();
+
+// Favicon
+/*var favicon  = document.createElement("link");
+favicon.rel  = "shortcut icon";
+favicon.type = "image/png";
+favicon.href = self.options.favicon; // icon-16.png
+document.head.appendChild(favicon);*/
